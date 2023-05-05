1e514ddbfdf3098d5aa4085d5511255f2c8ed63c
Michael Mfoundou <michael.mfoundou@yahoo.fr>
2023-05-05 19:32:52 +0200

    refactoring test with setUp function

--- app/src/test/java/com/example/circlcalc/MyCalcTest.kt
+++ app/src/test/java/com/example/circlcalc/MyCalcTest.kt
@@ -1,21 +1,26 @@
 package com.example.circlcalc
 
 import com.google.common.truth.Truth.assertThat
+import org.junit.Before
 import org.junit.Test
 
 class MyCalcTest{
  private lateinit var myCalc: MyCalc
 
-  @Test
+ @Before
+ fun setUp() {
+  myCalc = MyCalc()
+
+ }
+
+ @Test
   fun calculateCircumference_radiusGiven_returnsCorrectResult(){
-   myCalc = MyCalc()
    val result = myCalc.calculateCircumference(2.1)
    assertThat(result).isEqualTo(13.188)
   }
 
  @Test
  fun calculateCircumference_zeroRadius_returnsCorrectResult(){
-  myCalc = MyCalc()
   val result = myCalc.calculateCircumference(0.0)
   assertThat(result).isEqualTo(.00)
  }
